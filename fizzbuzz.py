7443ade14ec6dda516229c85ca70bfae4ea5b719
Jordan White <jordanwhite268@gmail.com>
2023-10-28 20:37:20 -0400

    added fizzbuzz code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,9 @@
-# add your code here
+n = n + 1
+if (n % 5 == 0) and (n % 3 == 0):
+    print ("FizzBuzz")
+elif n % 5 == 0:
+    print ("Buzz")
+elif (n % 3 == 0):
+    print("Fizz")
 
+else: print(n)
